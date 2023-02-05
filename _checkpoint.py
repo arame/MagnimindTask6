0ce671df4bf3c450c46cbcf211f94a55072cebe1
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2023-02-05 22:00:23 +0000

    Logging does not work so replace with print

--- _checkpoint.py
+++ _checkpoint.py
@@ -14,26 +14,26 @@ class SaveBestModel:
     ):
         self.best_valid_accuracy = best_valid_accuracy
         self.checkpoint_path = checkpoint_path
-        
+
     def __call__(
         self, current_valid_accuracy, epoch, model, optimizer, criterion
     ):
         if current_valid_accuracy > self.best_valid_accuracy:
             self.best_valid_accuracy = current_valid_accuracy
-            print(f"\nBest validation accuracy: {self.best_valid_accuracy}")
-            print(f"\nSaving best model for epoch: {epoch+1}\n")
+            print(f"Best validation accuracy: {self.best_valid_accuracy}")
+            print(f"Saving best model for epoch: {epoch}")
             torch.save({
-                'epoch': epoch+1,
+                'epoch': epoch,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': criterion,
                 }, self.checkpoint_path)
 
-def save_model(epochs, model, optimizer, criterion, final_checkpoint_path):
+def save_model(epochs, model, optimizer, criterion, final_checkpoint_path, logging):
     """
     Function to save the trained model to disk.
     """
-    print(f"Saving final model...")
+    logging(f"Saving final model...")
     torch.save({
                 'epoch': epochs,
                 'model_state_dict': model.state_dict(),
